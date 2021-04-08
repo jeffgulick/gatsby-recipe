a10a5229c193d54e2379dca04363793211b701d4
JEFFGULICK <jeffgulick@gmail.com>
2021-04-08 00:43:39 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,43 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `1sfeg2xcbtuz`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: `1nbHjd66EXSx6nNgEdRPZzTYUbDlk_oQl9OsjlkeiWA`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Montserrat",
+              variants: ["400"],
+            },
+            {
+              family: "Inconsolata",
+              variants: ["400", "500", "600", "700"],
+            },
+          ],
+        },
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+  ],
 }
---
+++ src/pages/tags.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const tags = () => {
+    return (
+        <Layout>
+            tags
+        </Layout>
+    )
+}
+
+export default tags
