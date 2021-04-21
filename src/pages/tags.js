91ecd8e64e237f5660b8732452860813d6b4d4f8
JEFFGULICK <jeffgulick@gmail.com>
2021-04-20 22:20:55 -0500

    set up tags page

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
+import { graphql, Link } from "gatsby";
+import setUpTags from "../utils/setUpTags";
 
-const Tags = () => {
-    return (
-        <Layout>
-            tags
-        </Layout>
-    )
-}
+const Tags = ({ data }) => {
+  const newTags = setUpTags(data.allContentfulRecipe.nodes);
+  return (
+    <Layout>
+      <main className="page">
+        <section className="tags-page">
+          {newTags.map((tag, index) => {
+            const [text, value] = tag;
+            return (
+              <Link key={index} to={`/${text}`} className="tag">
+                <h5>{text}</h5>
+                <p>{value} recipe</p>
+              </Link>
+            );
+          })}
+        </section>
+      </main>
+    </Layout>
+  );
+};
 
-export default Tags
+//page query that gets tags from contentful
+export const query = graphql`
+  {
+    allContentfulRecipe {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`;
+export default Tags;
