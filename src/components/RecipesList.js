af68baac4c2a597ae3411ba0cebfe6e6e46a8626
JEFFGULICK <jeffgulick@gmail.com>
2021-04-20 22:51:31 -0500

    fixed url bug with slugify for recipe page nav

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import slugify from "slugify";
 
 //component the adds recipe images and titles to page
 const RecipesList = ({ recipes = [] }) => {
@@ -11,9 +12,10 @@ const RecipesList = ({ recipes = [] }) => {
         const { id, title, image, prepTime, cookTime } = recipe;
         //helper function for image. prevents crash if image is missing
         const path = getImage(image);
+        const slug = slugify(title, { lower: true });
         return (
           //setting up images with titles
-          <Link key={id} to={`/${title}`} className="recipe">
+          <Link key={id} to={`/${slug}`} className="recipe">
             <GatsbyImage image={path} className="recipe-img" alt={title} />
             <h5>{title}</h5>
             <p>
